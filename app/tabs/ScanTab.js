d5c26c8b5fae6e2ce8dcdef11534988250812027
Blake Hieu Nguyen <57298440+nvthblake@users.noreply.github.com>
2020-10-15 00:24:06 -0400

    Add Scan Tab

---
+++ app/tabs/ScanTab.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import * as Yup from "yup";
+import { Formik } from "formik";
+
+import {
+  AppForm,
+  AppFormField,
+  AppFormPicker,
+  SubmitButton,
+} from "../components/forms";
+import Screen from "../components/Screen";
+import CategoryPickerItem from "../components/CategoryPickerItem";
+import FormImagePicker from "../components/forms/FormImagePicker";
+
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required().min(1).label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
+  description: Yup.string().label("Description"),
+  category: Yup.object().required().nullable().label("Category"),
+  image: Yup.array().min(1, "Please select at least 1 image."),
+});
+
+const categories = [
+  { label: "Meat", value: 1, backgroundColor: "red", icon: "apps" },
+  { label: "Vegetable", value: 2, backgroundColor: "green", icon: "email" },
+  { label: "Condiments", value: 3, backgroundColor: "blue", icon: "lock" },
+  { label: "Snack", value: 4, backgroundColor: "blue", icon: "lock" },
+  { label: "Fruit", value: 5, backgroundColor: "blue", icon: "lock" },
+  { label: "Others", value: 6, backgroundColor: "blue", icon: "lock" },
+];
+
+const units = [
+  { label: "Quartz", value: 1},
+  { label: "Kg", value: 2}
+]
+
+function ScanTab() {
+ 
+ 
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{
+          ingredient: "",
+          qty: "",
+          unit: "",
+          category: null,
+          dayToExp: "",
+          images: [],
+        }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <FormImagePicker name="images" />
+        <AppFormField maxLength={255} name="ingredient" placeholder="Ingredient" />
+        <AppFormField
+          keyboardType="numeric"
+          maxLength={8}
+          name="qty"
+          placeholder="Quantity"
+        />
+        <AppFormPicker
+          items={units}
+          name="unit"
+          placeholder="Unit"
+        />
+        <AppFormPicker
+          items={categories}
+          name="category"
+          // numberOfColumns={3} // Comment this out to display only 1 column
+          // PickerItemComponent={CategoryPickerItem} // comment this out if dont want to display icon
+          placeholder="Category"
+        />
+        <AppFormField
+          maxLength={255}
+          multiline
+          name="description"
+          numberOfLines={3}
+          placeholder="Description"
+        />
+        <SubmitButton title="Post" />
+      </AppForm>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default ScanTab;
