c5045cc0af22357303090e668bf22cbbe0b427e4
Quang Luong <quanglddev@gmail.com>
2020-10-31 18:50:27 -0400

    Finish 1/2 of recipe screen

---
+++ app/config/secret.js
@@ -0,0 +1,6 @@
+const SPOONACULAR_SECRETS = {
+    "X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "X-RapidAPI-Key": "895ce719e4mshcb836fa18684a5ap1c69f2jsnf7e37492c80d"
+}
+
+module.exports = { SPOONACULAR_SECRETS }
