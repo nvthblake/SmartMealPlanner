ab5ae7586ec60c21a7563f72712285b1404aa331
Nhi <mainhitranthien@gmail.com>
2020-11-01 11:58:50 -0500

    Scan Tab: put image into App Form. Can submit form.

---
+++ app/components/forms/FormImageStatic.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Image,Dimensions } from "react-native";
+import { useFormikContext } from "formik";
+
+import ErrorMessage from "./ErrorMessage";
+import ImageInputList from "../ImageInputList";
+
+function FormImageStatic({ name }) {
+  const { values } = useFormikContext();
+  const imageUris = values[name];
+
+  const screenWidth = Dimensions.get("window").width;
+  console.log(values);
+
+
+  return (
+    <>
+      <Image
+        style={{
+            width: "100%",
+            height: 0.5 * screenWidth,
+            borderRadius: 25,
+        }}
+        source={{ uri: imageUris }}
+      />
+    </>
+  );
+}
+
+export default FormImageStatic;
