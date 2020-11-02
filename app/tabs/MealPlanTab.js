a0efc84d36ab4a73afc3f21bb296f9351f5bd7e4
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-11-02 17:06:31 -0500

    Change whole calendar to calendar strip

--- app/tabs/MealPlanTab.js
+++ app/tabs/MealPlanTab.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import React, { Component, Fragment } from "react";
+import { StyleSheet, View, Text, Dimensions, SafeAreaView } from "react-native";
+import CalendarStrip from 'react-native-calendar-strip';
+import moment from 'moment';
 
 // import { connect } from "react-redux";
 // import { bindActionCreators } from "redux";
@@ -9,6 +10,7 @@ import Screen from "../components/Screen";
 import colors from "../config/colors";
 
 import { addRecipe, clearRecipe } from "../../actions";
+import { color } from "react-native-reanimated";
 
 
 class MealPlanTab extends Component {
@@ -17,43 +19,51 @@ class MealPlanTab extends Component {
         this.state = {
             date: new Date(),
         }
+        let datesWhitelist = [{
+            start: moment(),
+            end: moment().add(3, 'days')  // total 4 days enabled
+        }];
+        let datesBlacklist = [moment().add(1, 'days')]; // 1 day disabled
     }
 
     render() {
         return (
-            <Screen style={styles.screen}>
-                <View style={styles.container}>
-                    <View style={styles.body}>
-                        <View>
-                            <Text style={styles.title}>
-                                Meal Planning
-                            </Text>
-                        </View>
-
-                        <Calendar
-                            style={styles.calendar}
-                            markingType={'period'}
-                            markedDates={{
-                                '2020-10-15': { marked: true, dotColor: colors.primary },
-                                '2020-10-16': { marked: true, dotColor: colors.primary },
-                                '2020-10-21': { startingDay: true, color: colors.primary, textColor: 'white' },
-                                '2020-10-22': { color: colors.light_primary, textColor: 'white' },
-                                '2020-10-23': { color: colors.light_primary, textColor: 'white', marked: true, dotColor: 'white' },
-                                '2020-10-24': { color: colors.light_primary, textColor: 'white' },
-                                '2020-10-25': { endingDay: true, color: colors.primary, textColor: 'white' },
-                            }}
+            <Fragment>
+                <SafeAreaView style={{ flex: 0, backgroundColor: colors.primary }} />
+                <Screen style={styles.screen}>
+                    <View style={styles.container}>
+                        <CalendarStrip
+                            calendarAnimation={{ type: 'sequence', duration: 30 }}
+                            daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white' }}
+                            style={{ height: 100, paddingBottom: 10 }}
+                            calendarHeaderStyle={{ color: 'white' }}
+                            calendarColor={colors.primary}
+                            dateNumberStyle={{ color: 'white' }}
+                            dateNameStyle={{ color: 'white' }}
+                            highlightDateNumberStyle={{ color: 'yellow' }}
+                            highlightDateNameStyle={{ color: 'yellow' }}
+                            disabledDateNameStyle={{ color: 'grey' }}
+                            disabledDateNumberStyle={{ color: 'grey' }}
+                            datesWhitelist={this.datesWhitelist}
+                            datesBlacklist={this.datesBlacklist}
+                            // iconLeft={require('./img/left-arrow.png')}
+                            // iconRight={require('./img/right-arrow.png')}
+                            iconContainer={{ flex: 0.1 }}
                         />
+
+
                     </View>
-                </View>
-            </Screen>
+                </Screen>
+
+            </Fragment>
         );
     }
 }
 
 const styles = StyleSheet.create({
     screen: {
-        paddingTop: 8,
-        backgroundColor: colors.light,
+        paddingTop: 0,
+        backgroundColor: colors.backgroundColor,
         flex: 1,
     },
     container: {
@@ -63,9 +73,11 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     calendar: {
-        padding: 8,
-        margin: 25,
+        margin: 20,
         borderRadius: 20,
+
+    },
+    shadowBox: {
         // shadow
         shadowColor: colors.shadow,
         shadowOffset: {
