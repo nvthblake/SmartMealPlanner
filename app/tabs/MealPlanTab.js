609d7e0c9a5efa6e1eeeaebf8c331702925698a8
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-11-02 21:17:05 -0500

    Calender Strip: done marking current date, 2 weeks highlight color

--- app/tabs/MealPlanTab.js
+++ app/tabs/MealPlanTab.js
@@ -1,63 +1,106 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment, useState } from "react";
 import { StyleSheet, View, Text, Dimensions, SafeAreaView } from "react-native";
 import CalendarStrip from 'react-native-calendar-strip';
 import moment from 'moment';
 
-// import { connect } from "react-redux";
-// import { bindActionCreators } from "redux";
+// Redux
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import {
+    addMealPlan, deleteMealPlan, clearMealPlan,
+    addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
+} from "../../actions";
 
+// Components
+import LoadingAnimation from '../components/LoadingAnimation';
 import Screen from "../components/Screen";
+
+// Misc
 import colors from "../config/colors";
+import { ScrollView } from "react-native-gesture-handler";
 
-import { addRecipe, clearRecipe } from "../../actions";
-import { color } from "react-native-reanimated";
+/* Copied from IngredientsTab */
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+function MealPlanTab(state) {
+    const {
+        mealPlanner,
+        favoriteRecipes,
+        addMealPlan, deleteMealPlan, clearRecipe,
+        addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
+    } = state;
+    const [isLoading, setIsLoading] = useState(true);
+    const curDate = new Date();
+
+    let datesWhitelist = [{
+        start: moment(),
+        end: moment().add(13, 'days')  // total 2 weeks
+    }];
 
 
-class MealPlanTab extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-        }
-        let datesWhitelist = [{
-            start: moment(),
-            end: moment().add(3, 'days')  // total 4 days enabled
-        }];
-        let datesBlacklist = [moment().add(1, 'days')]; // 1 day disabled
+    const getMealPlanOnDate = (date) => {
+        const mealPlan = [];
+
+        return mealPlan;
     }
 
-    render() {
-        return (
-            <Fragment>
-                <SafeAreaView style={{ flex: 0, backgroundColor: colors.primary }} />
-                <Screen style={styles.screen}>
-                    <View style={styles.container}>
-                        <CalendarStrip
-                            calendarAnimation={{ type: 'sequence', duration: 30 }}
-                            daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white' }}
-                            style={{ height: 100, paddingBottom: 10 }}
-                            calendarHeaderStyle={{ color: 'white' }}
-                            calendarColor={colors.primary}
-                            dateNumberStyle={{ color: 'white' }}
-                            dateNameStyle={{ color: 'white' }}
-                            highlightDateNumberStyle={{ color: 'yellow' }}
-                            highlightDateNameStyle={{ color: 'yellow' }}
-                            disabledDateNameStyle={{ color: 'grey' }}
-                            disabledDateNumberStyle={{ color: 'grey' }}
-                            datesWhitelist={this.datesWhitelist}
-                            datesBlacklist={this.datesBlacklist}
-                            // iconLeft={require('./img/left-arrow.png')}
-                            // iconRight={require('./img/right-arrow.png')}
-                            iconContainer={{ flex: 0.1 }}
-                        />
+    // const getFavoriteRecipes = () => {
+    //     const recipes = []
 
+    //     return recipes;
+    // }
 
-                    </View>
-                </Screen>
+    const selectedMealPlan = getMealPlanOnDate(curDate);
+    // const favoriteRecipes = getFavoriteRecipes();
 
-            </Fragment>
-        );
+    const onChange = (e) => {
+
     }
+
+    const markedCurDate = [
+        {
+            date: curDate,
+            dots: [
+                {
+                    color: 'white',
+                },
+            ],
+        },
+    ];
+
+    return (
+        <Fragment>
+            <SafeAreaView style={{ flex: 0, backgroundColor: colors.primary }} />
+            <Screen style={styles.screen}>
+
+                {/* Calendar */}
+                <View style={styles.container}>
+                    <CalendarStrip
+                        calendarAnimation={{ type: 'sequence', duration: 30 }}
+                        daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 2, borderHighlightColor: 'white' }}
+                        style={{ height: 100, paddingBottom: 10 }}
+                        calendarHeaderStyle={{ color: 'white' }}
+                        calendarColor={colors.primary}
+                        dateNumberStyle={{ color: 'white' }}
+                        dateNameStyle={{ color: 'white' }}
+                        highlightDateNumberStyle={{ color: 'white' }}
+                        highlightDateNameStyle={{ color: 'white' }}
+                        disabledDateNameStyle={{ color: 'black' }}
+                        disabledDateNumberStyle={{ color: 'black' }}
+                        datesWhitelist={datesWhitelist}
+                        // iconLeft={require('./img/left-arrow.png')}
+                        // iconRight={require('./img/right-arrow.png')}
+                        iconContainer={{ flex: 0.1 }}
+                        markedDates={markedCurDate}
+                    />
+                </View>
+
+
+            </Screen>
+
+        </Fragment>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -75,7 +118,6 @@ const styles = StyleSheet.create({
     calendar: {
         margin: 20,
         borderRadius: 20,
-
     },
     shadowBox: {
         // shadow
@@ -97,5 +139,19 @@ const styles = StyleSheet.create({
     }
 });
 
+const mapStateToProps = (state) => {
+    const { mealPlanner, favoriteRecipes } = state;
+    return { mealPlanner, favoriteRecipes };
+};
 
-export default MealPlanTab;
+const mapDispatchToProps = (dispatch) =>
+    bindActionCreators(
+        {
+            addMealPlan, deleteMealPlan, clearMealPlan,
+            addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
+        },
+        dispatch
+    );
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(MealPlanTab);
