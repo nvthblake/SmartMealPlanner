b7ae3a2c3af2e593efb4a825a0cd523587b2b30b
Ari Nguyen <49668914+AriNguyen@users.noreply.github.com>
2020-11-04 20:12:59 -0500

    add redux and recipes card to MealPlan

--- app/tabs/MealPlanTab.js
+++ app/tabs/MealPlanTab.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, useState } from "react";
+import React, { Component, Fragment, useEffect, useState } from "react";
 import { StyleSheet, View, Text, Dimensions, SafeAreaView } from "react-native";
 import CalendarStrip from 'react-native-calendar-strip';
 import moment from 'moment';
@@ -7,13 +7,16 @@ import moment from 'moment';
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import {
-    addMealPlan, deleteMealPlan, clearMealPlan,
-    addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
+  addMealPlan, deleteMealPlan, clearMealPlan,
+  addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
 } from "../../actions";
 
 // Components
 import LoadingAnimation from '../components/LoadingAnimation';
 import Screen from "../components/Screen";
+
+// API
+import { getRecipes, getRecipeInfoInBulk } from '../api/Spoonacular';
 
 // Misc
 import colors from "../config/colors";
@@ -23,135 +26,250 @@ import { ScrollView } from "react-native-gesture-handler";
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
+const INITIAL_CATEGORIES_STATE = [
+  {
+    title: "All",
+    selected: true
+  }
+];
+
+
 function MealPlanTab(state) {
-    const {
-        mealPlanner,
-        favoriteRecipes,
-        addMealPlan, deleteMealPlan, clearRecipe,
-        addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
-    } = state;
-    const [isLoading, setIsLoading] = useState(true);
-    const curDate = new Date();
+  // Database data
+  const {
+    ingredients,
+    addMealPlan, deleteMealPlan, clearRecipe,
+    addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
+  } = state;
+  const ingredientsInFridge = ingredients.fridge;
+  const recipes = ingredients.recipes;
+  const mealPlanner = ingredients.mealPlanner;
+  const favoriteRecipes = ingredients.favoriteRecipes;
 
-    let datesWhitelist = [{
-        start: moment(),
-        end: moment().add(13, 'days')  // total 2 weeks
-    }];
+  // Vars related to calendar
+  const curDate = new Date();
+  let datesWhitelist = [{
+    start: moment(),
+    end: moment().add(13, 'days')  // total 2 weeks
+  }];
+  const markedCurDate = [
+    {
+      date: curDate,
+      dots: [
+        {
+          color: 'white',
+        },
+      ],
+    },
+  ];
 
+  // State vars
+  const [isLoading, a ] = useState(true);
+  const [categories, setCategory] = useState(INITIAL_CATEGORIES_STATE);
+  const numMealPlans = 14;
 
-    const getMealPlanOnDate = (date) => {
-        const mealPlan = [];
+  const generateMealPlan = () => {
+    const mealPlans = [];
 
-        return mealPlan;
+    // no meal plan in database
+    if (mealPlanner.length() == 0) {
+      ;
     }
+    // meal plans in database is < numDay 
+    else if (mealPlanner.length() < numMealPlans) {
+      const neededNumMealPlan = numMealPlans - mealPlanner.length();
+
+    }
+    // meal plan > numdate
+    else {
+      const neededNumMealPlan = numMealPlans - mealPlanner.length();
+
+    }
+
+    return mealPlans;
+  }
+
+  const getMealPlanOnDate = (date) => {
+    const mealPlan = [];
 
     // const getFavoriteRecipes = () => {
     //     const recipes = []
 
     //     return recipes;
-    // }
+  }
 
-    const selectedMealPlan = getMealPlanOnDate(curDate);
-    // const favoriteRecipes = getFavoriteRecipes();
+  const selectedMealPlan = getMealPlanOnDate(curDate);
+  // const favoriteRecipes = getFavoriteRecipes();
 
-    const onChange = (e) => {
+  const onChange = (e) => {
 
+  }
+
+  const getRecipesBasedOnFilter = (recipes) => {
+    const result = [];
+    const selectedCategories = categories.filter((category => category.selected)).map((category) => category.title);
+    if (selectedCategories.indexOf("All") > -1) {
+      return recipes;
     }
+    for (let i = 0; i < recipes.length; i++) {
+      for (let j = 0; j < selectedCategories.length; j++) {
+        if (recipes[i].dishTypes.indexOf(selectedCategories[j]) > -1) {
+          result.push(recipes[i]);
+          break;
+        }
+      }
+    }
+    return result;
+  };
 
-    const markedCurDate = [
-        {
-            date: curDate,
-            dots: [
-                {
-                    color: 'white',
-                },
-            ],
-        },
-    ];
+  useEffect(() => {
+    getRecipes(ingredientsInFridge, numMealPlans).then((step1_recipes) => {
+      getRecipeInfoInBulk(step1_recipes).then((final_recipes) => {
+        final_recipes.sort((a, b) => {
+          return b.likes - a.likes;
+        });
+        clearRecipe();
 
-    return (
-        <Fragment>
-            <SafeAreaView style={{ flex: 0, backgroundColor: colors.primary }} />
-            <Screen style={styles.screen}>
+        let allDishTypes = [];
+        final_recipes.forEach(recipe => {
+          addRecipe(recipe);
+          recipe.dishTypes.forEach(dishType => {
+            allDishTypes.push(dishType);
+          });
+        });
+        resetCategories();
+        allDishTypes = Array.from(new Set(allDishTypes));
+        console.log("RecipeTab -> allDishTypes", allDishTypes)
+        addCategories(allDishTypes);
+        console.log("RecipeTab -> recipes", recipes.length)
+        setIsLoading(false);
+      });
+    });
+  })
 
-                {/* Calendar */}
-                <View style={styles.container}>
-                    <CalendarStrip
-                        calendarAnimation={{ type: 'sequence', duration: 30 }}
-                        daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 2, borderHighlightColor: 'white' }}
-                        style={{ height: 100, paddingBottom: 10 }}
-                        calendarHeaderStyle={{ color: 'white' }}
-                        calendarColor={colors.primary}
-                        dateNumberStyle={{ color: 'white' }}
-                        dateNameStyle={{ color: 'white' }}
-                        highlightDateNumberStyle={{ color: 'white' }}
-                        highlightDateNameStyle={{ color: 'white' }}
-                        disabledDateNameStyle={{ color: 'black' }}
-                        disabledDateNumberStyle={{ color: 'black' }}
-                        datesWhitelist={datesWhitelist}
-                        // iconLeft={require('./img/left-arrow.png')}
-                        // iconRight={require('./img/right-arrow.png')}
-                        iconContainer={{ flex: 0.1 }}
-                        markedDates={markedCurDate}
-                    />
+  const filteredRecipes = getRecipesBasedOnFilter(recipes);
+  const veryPopularRecipes = filteredRecipes.filter((recipe) => recipe.veryPopular);
+
+  return (
+    <Fragment>
+      <SafeAreaView style={{ flex: 0, backgroundColor: colors.primary }} />
+      <Screen style={styles.screen}>
+
+        {/* Calendar */}
+        <View style={styles.container}>
+          <CalendarStrip
+            calendarAnimation={{ type: 'sequence', duration: 30 }}
+            daySelectionAnimation={{ type: 'border', duration: 200, borderWidth: 2, borderHighlightColor: 'white' }}
+            style={{ height: 100, paddingBottom: 10 }}
+            calendarHeaderStyle={{ color: 'white' }}
+            calendarColor={colors.primary}
+            dateNumberStyle={{ color: 'white' }}
+            dateNameStyle={{ color: 'white' }}
+            highlightDateNumberStyle={{ color: 'white' }}
+            highlightDateNameStyle={{ color: 'white' }}
+            disabledDateNameStyle={{ color: 'black' }}
+            disabledDateNumberStyle={{ color: 'black' }}
+            datesWhitelist={datesWhitelist}
+            // iconLeft={require('./img/left-arrow.png')}
+            // iconRight={require('./img/right-arrow.png')}
+            iconContainer={{ flex: 0.1 }}
+            markedDates={markedCurDate}
+          />
+        </View>
+
+        {/* Meal Plan */}
+        {isLoading && <View style={{ width: screenWidth, height: screenHeight / 1.5 }}><LoadingAnimation show={isLoading} label={'Finding the best recipes for you...'} /></View>}
+        {!isLoading &&
+          <ScrollView>
+            {veryPopularRecipes.length > 0 &&
+              <View>
+                <View style={{ padding: 16 }}>
+                  <Text style={{ fontSize: 22, fontWeight: 'bold' }}>Very Popular</Text>
                 </View>
+                <View>
+                  <FlatList
+                    data={veryPopularRecipes}
+                    horizontal={true}
+                    keyExtractor={(recipe) => recipe.id.toString()}
+                    renderItem={({ recipe, index }) => {
+                      return (
+                        <View style={{ paddingBottom: 5 }}>
+                          <View style={styles.recipeCard}>
+                            <TouchableOpacity onPress={() => openURLInDefaultBrowser(veryPopularRecipes[index].sourceUrl)}>
+                              <View style={{ padding: 10 }}>
+                                <View style={{ flexDirection: 'column' }}>
+                                  <Image source={{ uri: veryPopularRecipes[index].image }} style={{ width: '100%', marginRight: 14, height: 140, borderRadius: 10, marginRight: 8 }}></Image>
+                                  <Text numberOfLines={2} style={styles.recipeTitle}>{veryPopularRecipes[index].title}</Text>
+                                  <Text numberOfLines={1} style={styles.recipeLikes}>{nFormatter(veryPopularRecipes[index].likes, 1)} likes</Text>
+                                  <Text numberOfLines={1} style={styles.recipeUsedIngredients}>{veryPopularRecipes[index].usedIngredients.length} ingredients</Text>
+                                  <Text numberOfLines={1} style={styles.recipeMissingIngredients}>{veryPopularRecipes[index].missedIngredients.length} missings</Text>
+                                </View>
+                              </View>
+                            </TouchableOpacity>
+                          </View>
+                        </View>
+                      )
+                    }}>
+                  </FlatList>
+                </View>
+              </View>}
+          </ScrollView>
+        }
+      </Screen>
 
-
-            </Screen>
-
-        </Fragment>
-    );
+    </Fragment>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        paddingTop: 0,
-        backgroundColor: colors.backgroundColor,
-        flex: 1,
+  screen: {
+    paddingTop: 0,
+    backgroundColor: colors.backgroundColor,
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+  },
+  calendar: {
+    margin: 20,
+    borderRadius: 20,
+  },
+  shadowBox: {
+    // shadow
+    shadowColor: colors.shadow,
+    shadowOffset: {
+      width: 0,
+      height: 4,
     },
-    container: {
-        flex: 1,
-    },
-    body: {
-        flex: 1,
-    },
-    calendar: {
-        margin: 20,
-        borderRadius: 20,
-    },
-    shadowBox: {
-        // shadow
-        shadowColor: colors.shadow,
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 1,
-        shadowRadius: 10,
-        elevation: 9
-    },
-    title: {
-        // fontStyle: 'Avenir',
-        textAlign: 'center', // <-- the magic
-        fontWeight: 'bold',
-        fontSize: 28,
-        color: colors.font_dark
-    }
+    shadowOpacity: 1,
+    shadowRadius: 10,
+    elevation: 9
+  },
+  title: {
+    // fontStyle: 'Avenir',
+    textAlign: 'center', // <-- the magic
+    fontWeight: 'bold',
+    fontSize: 28,
+    color: colors.font_dark
+  }
 });
 
 const mapStateToProps = (state) => {
-    const { mealPlanner, favoriteRecipes } = state;
-    return { mealPlanner, favoriteRecipes };
+  const { ingredients } = state;
+  return { ingredients };
 };
 
 const mapDispatchToProps = (dispatch) =>
-    bindActionCreators(
-        {
-            addMealPlan, deleteMealPlan, clearMealPlan,
-            addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
-        },
-        dispatch
-    );
+  bindActionCreators(
+    {
+      addMealPlan, deleteMealPlan, clearMealPlan,
+      addFavoriteRecipe, deleteFavoriteRecipe, clearFavoriteRecipe
+    },
+    dispatch
+  );
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(MealPlanTab);
