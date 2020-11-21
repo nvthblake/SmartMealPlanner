75d5aa13bd3da8c88a630cbd3289676758288134
Blake Hieu Nguyen <57298440+nvthblake@users.noreply.github.com>
2020-11-21 11:42:59 -0500

    small changes

--- app/tabs/MealPlanTab.js
+++ app/tabs/MealPlanTab.js
@@ -41,7 +41,7 @@ import { getRecipes, getRecipeInfoInBulk } from "../api/Spoonacular";
 import colors from "../config/colors";
 import { ScrollView } from "react-native-gesture-handler";
 import { nFormatter } from "../utils/NumberFormatting";
-import { capitalize } from "../utils/TextFormatting"
+import { capitalize } from "../utils/TextFormatting";
 
 /* Copied from IngredientsTab */
 const screenWidth = Dimensions.get("window").width;
@@ -89,7 +89,7 @@ function MealPlanTab(state) {
         end: moment().add(num, "days"), // total 2 weeks
       },
     ];
-  }
+  };
   const markedCurDate = [
     {
       date: curDate,
@@ -103,24 +103,30 @@ function MealPlanTab(state) {
 
   const getDateHeader = (date) => {
     console.log("date: ", date, curDate);
-    var msDateA = Date.UTC(date.getFullYear(), date.getMonth()+1, date.getDate()); 
-    var msDateB = Date.UTC(curDate.getFullYear(), curDate.getMonth()+1, curDate.getDate()); 
-    moment.locale('en');
+    var msDateA = Date.UTC(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      date.getDate()
+    );
+    var msDateB = Date.UTC(
+      curDate.getFullYear(),
+      curDate.getMonth() + 1,
+      curDate.getDate()
+    );
+    moment.locale("en");
 
     if (msDateA == msDateB) {
       return "Today's Meal Plan";
+    } else {
+      return moment(date).format("MMM D") + " Meal Plan";
     }
-    else {
-      return moment(date).format('MMM D') + " Meal Plan";
-    }
-    
-  }
+  };
 
   const onDateSelect = (date) => {
     console.log(date);
     let d = new Date(date);
     setSelectDate(d);
-    var msDiff = new Date(date).getTime() - new Date().getTime();    //Future date - current date
+    var msDiff = new Date(date).getTime() - new Date().getTime(); //Future date - current date
     var index = Math.floor(msDiff / (1000 * 60 * 60 * 24)) + 1;
     console.log(index);
     if (mealPlan[index] != undefined) {
@@ -128,7 +134,7 @@ function MealPlanTab(state) {
       getMealPlanOnDate(mealPlan[index]);
     }
     return date;
-  }
+  };
 
   // Recipes
   const getRecipesBasedOnFilter = (recipes) => {
@@ -158,39 +164,52 @@ function MealPlanTab(state) {
     let lunchRecipes = [];
     let dinnerRecipes = [];
 
-    let breakfast = filteredRecipes.filter((recipe) => recipe.dishTypes.indexOf("breakfast") > -1);
-    let main_course = filteredRecipes.filter((recipe) => recipe.dishTypes.indexOf("main course") > -1);
-    let salad = filteredRecipes.filter((recipe) => recipe.dishTypes.indexOf("salad") > -1);
-    let soup = filteredRecipes.filter((recipe) => recipe.dishTypes.indexOf("soup") > -1);
-    let sauce = filteredRecipes.filter((recipe) => recipe.dishTypes.indexOf("sauce") > -1);
+    let breakfast = filteredRecipes.filter(
+      (recipe) => recipe.dishTypes.indexOf("breakfast") > -1
+    );
+    let main_course = filteredRecipes.filter(
+      (recipe) => recipe.dishTypes.indexOf("main course") > -1
+    );
+    let salad = filteredRecipes.filter(
+      (recipe) => recipe.dishTypes.indexOf("salad") > -1
+    );
+    let soup = filteredRecipes.filter(
+      (recipe) => recipe.dishTypes.indexOf("soup") > -1
+    );
+    let sauce = filteredRecipes.filter(
+      (recipe) => recipe.dishTypes.indexOf("sauce") > -1
+    );
 
     breakfast.forEach(function (recipe) {
-      if (breakfastRecipes.find(e => e.id === recipe.id) == false) {
-        breakfastRecipes.push(recipe)
+      if (breakfastRecipes.find((e) => e.id === recipe.id) == false) {
+        breakfastRecipes.push(recipe);
       }
-    })
+    });
 
     salad.forEach(function (recipe) {
-      if (breakfastRecipes.some(e => e.id === recipe.id) == false) {
-        breakfastRecipes.push(recipe)
+      if (breakfastRecipes.some((e) => e.id === recipe.id) == false) {
+        breakfastRecipes.push(recipe);
       }
-    })
+    });
     sauce.forEach(function (recipe) {
-      if (breakfastRecipes.some(e => e.id === recipe.id) == false) {
-        breakfastRecipes.push(recipe)
+      if (breakfastRecipes.some((e) => e.id === recipe.id) == false) {
+        breakfastRecipes.push(recipe);
       }
-    })
+    });
     soup.forEach(function (recipe) {
-      if (breakfastRecipes.some(e => e.id === recipe.id) == false) {
-        breakfastRecipes.push(recipe)
+      if (breakfastRecipes.some((e) => e.id === recipe.id) == false) {
+        breakfastRecipes.push(recipe);
       }
-    })
+    });
 
     console.log("------main_course");
     console.log(Object.keys(main_course));
 
     lunchRecipes = main_course.slice(0, Math.ceil(main_course.length / 2));
-    dinnerRecipes = main_course.slice(Math.ceil(main_course.length / 2), main_course.length);
+    dinnerRecipes = main_course.slice(
+      Math.ceil(main_course.length / 2),
+      main_course.length
+    );
 
     console.log("------breakfastRecipes");
     console.log(Object.keys(breakfastRecipes));
@@ -200,14 +219,22 @@ function MealPlanTab(state) {
     console.log(Object.keys(dinnerRecipes));
 
     let mealPlan = {};
-    maxlength = Math.max(breakfastRecipes.length, lunchRecipes.length, dinnerRecipes.length);
-    const minlength = Math.min(breakfastRecipes.length, lunchRecipes.length, dinnerRecipes.length);
+    maxlength = Math.max(
+      breakfastRecipes.length,
+      lunchRecipes.length,
+      dinnerRecipes.length
+    );
+    const minlength = Math.min(
+      breakfastRecipes.length,
+      lunchRecipes.length,
+      dinnerRecipes.length
+    );
 
     console.log("-----maxlength");
     console.log(maxlength);
     console.log("-----minlength");
     console.log(minlength);
-    
+
     console.log("here", Math.ceil(main_course.length / 2));
     console.log("here", Math.floor(main_course.length / 2));
     for (var i = 0; i < maxlength; i++) {
@@ -227,7 +254,7 @@ function MealPlanTab(state) {
       mealPlan[i] = [b, l, d];
     }
     return mealPlan;
-  }
+  };
   let mealPlan = generateMealPlan();
   const [selectMealPlan, getMealPlanOnDate] = useState(mealPlan[0]);
 
@@ -268,7 +295,7 @@ function MealPlanTab(state) {
           onPress: () => {
             console.log(recipe);
             // Delete from reduce
-            deleteMealPlan(recipe)
+            deleteMealPlan(recipe);
           },
         },
         {
@@ -305,7 +332,7 @@ function MealPlanTab(state) {
           highlightColor: colors.secondary,
           borderHighlightColor: "white",
         }}
-        style={{ height: 90, paddingTop: 10, marginBottom: 10}}
+        style={{ height: 90, paddingTop: 10, marginBottom: 10 }}
         calendarHeaderStyle={{ color: "black" }}
         calendarColor={"white"}
         dateNumberStyle={{ color: "black" }}
@@ -322,7 +349,6 @@ function MealPlanTab(state) {
         onDateSelected={onDateSelect}
         selectedDate={curDate}
       />
-
 
       {/* Today's Meal Plan */}
       {isLoading && (
@@ -352,7 +378,11 @@ function MealPlanTab(state) {
                   keyExtractor={(recipe) => recipe.id.toString()}
                   renderItem={({ recipe, index }) => {
                     return (
-                      <RecipeCard header={header[index]} recipe={selectMealPlan[index]} setChosenRecipeFunc={setChosenRecipe} />
+                      <RecipeCard
+                        header={header[index]}
+                        recipe={selectMealPlan[index]}
+                        setChosenRecipeFunc={setChosenRecipe}
+                      />
                     );
                   }}
                 ></FlatList>
@@ -370,16 +400,19 @@ function MealPlanTab(state) {
 
               {/* Recipe Cards */}
               <FlatList
-                  style={styles.recipeScroll}
-                  data={favoriteRecipes}
-                  horizontal={true}
-                  keyExtractor={(recipe) => recipe.id.toString()}
-                  renderItem={({ recipe, index }) => {
-                    return (
-                      <RecipeCard recipe={favoriteRecipes[index]} setChosenRecipeFunc={setChosenRecipe} />
-                    );
-                  }}
-                ></FlatList>
+                style={styles.recipeScroll}
+                data={favoriteRecipes}
+                horizontal={true}
+                keyExtractor={(recipe) => recipe.id.toString()}
+                renderItem={({ recipe, index }) => {
+                  return (
+                    <RecipeCard
+                      recipe={favoriteRecipes[index]}
+                      setChosenRecipeFunc={setChosenRecipe}
+                    />
+                  );
+                }}
+              ></FlatList>
               {/* <RecipeCard recipe={favoriteRecipes[0]} setChosenRecipeFunc={setChosenRecipe}/> */}
             </View>
           )}
@@ -408,24 +441,24 @@ function MealPlanTab(state) {
                   }}
                 ></Image>
                 <TouchableOpacity
-                onPress={() => {
-                  chosenRecipe.loved = !chosenRecipe.loved;
-                  console.log(chosenRecipe.loved);
-                  chosenRecipe.loved
-                    ? setHeartImage("heart") 
-                    : setHeartImage("heart-outline");
+                  onPress={() => {
+                    chosenRecipe.loved = !chosenRecipe.loved;
+                    console.log(chosenRecipe.loved);
                     chosenRecipe.loved
-                      ? addFavoriteRecipe(chosenRecipe) 
+                      ? setHeartImage("heart")
+                      : setHeartImage("heart-outline");
+                    chosenRecipe.loved
+                      ? addFavoriteRecipe(chosenRecipe)
                       : deleteFavoriteRecipe(chosenRecipe);
-                }}
-                style={{ position: "absolute" }}
-              >
-                <MaterialCommunityIcons
-                  name={chosenRecipe.loved ? "heart" : "heart-outline"}
-                  size={40}
-                  color={colors.font_red}
-                />
-              </TouchableOpacity>
+                  }}
+                  style={{ position: "absolute" }}
+                >
+                  <MaterialCommunityIcons
+                    name={chosenRecipe.loved ? "heart" : "heart-outline"}
+                    size={40}
+                    color={colors.font_red}
+                  />
+                </TouchableOpacity>
                 <Text
                   style={{
                     color: "#4F555E",
@@ -572,9 +605,7 @@ function MealPlanTab(state) {
                     borderRadius: 8,
                     paddingVertical: 8,
                   }}
-                  onPress={() =>
-                    handleDelete(chosenRecipe)
-                  }
+                  onPress={() => handleDelete(chosenRecipe)}
                 >
                   <Text
                     style={{
@@ -631,8 +662,8 @@ const styles = StyleSheet.create({
   },
   recipeScroll: {
     marginLeft: 6,
-    height: 240, 
-    paddingBottom: 10
+    height: 240,
+    paddingBottom: 10,
   },
   recipeTitle: {
     fontSize: 16,
