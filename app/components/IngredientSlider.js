4fee3ba6ce806ceef8950b0b1c92c498d90ca8e1
Blake Hieu Nguyen <57298440+nvthblake@users.noreply.github.com>
2020-11-23 11:25:54 -0500

    minor changes

--- app/components/IngredientSlider.js
+++ app/components/IngredientSlider.js
@@ -80,6 +80,7 @@ const styles = StyleSheet.create({
   },
   textContainer: {
     flexWrap: "wrap",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     alignSelf: "center",
